0e68078e869228f097797ea8052e60b5e397ecdc
Marc Howard <m.howard@live.com>
2017-11-19 12:42:42 +0000

    Add simple unit tests for controller and repository

---
+++ OvoTest.Tests/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OvoTest.Controllers;
+
+namespace OvoTest.Tests.Controllers
+{
+    [TestClass]
+    public class CustomerControllerTests
+    {
+        [TestMethod]
+        public void Customers()
+        {
+            // Arrange
+            CustomerController controller = new CustomerController();
+
+            // Act
+            ViewResult result = controller.Customers() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CustomerDetails()
+        {
+            // Arrange
+            CustomerController controller = new CustomerController();
+
+            // Act
+            ViewResult result = controller.CustomerDetails(new Guid("452e644a-8b21-4cfb-9683-f363be7aef6f")) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ OvoTest.Tests/Controllers/CustomerRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OvoTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvoTest.Tests.Controllers
+{
+    [TestClass]
+    public class CustomerRepositoryTests
+    {
+        private ICustomerRepository repo;
+
+        [TestInitialize]
+        public void setup()
+        {
+            //normally this would be mocked
+            repo = new CustomerRepository();
+        }
+
+        [TestMethod]
+        public void ShouldGetAllCustomers()
+        {
+            var result = repo.GetAllCustomers();
+
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<ICustomer>));
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldGetCustomerById()
+        {
+            var result = repo.GetCustomerById(new Guid());
+
+            Assert.IsInstanceOfType(result, typeof(ICustomer));
+            Assert.IsNotNull(result);
+        }
+    }
+}
